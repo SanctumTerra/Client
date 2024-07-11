3ce038b33c134192f9c028462f0c27f5d1e84989
AnyBananaGAME <ainaraspirmas@gmail.com>
2024-07-11 21:56:46 +0300

    Examples

---
+++ Examples/Test.js
@@ -0,0 +1,24 @@
+const { Client } = require("../");
+
+const client = new Client({
+    host: "127.0.0.1",
+    port: 19132
+});
+
+client.connect();
+
+client.on("spawn", () => {
+    setInterval(() => {
+        const Text = new TextPacket()
+        Text.filtered = "";
+        Text.message = ` ${new Date().toLocaleDateString() } `
+        Text.needsTranslation = false;
+        Text.parameters = [];
+        Text.platformChatId = "";
+        Text.source = _client.data.profile.name;
+        Text.type = TextPacketType.Chat;
+        Text.xuid = "";
+        _client.sendPacket(Text, Priority.Immediate);
+        
+    }, 10000);
+})
