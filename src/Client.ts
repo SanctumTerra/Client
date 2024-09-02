cff8890e584329838fbcff23ca992c546007d9f9
AnyBananaGAME <ainaraspirmas@gmail.com>
2024-09-02 21:41:45 +0300

    For some shelter and some records to play And so we pray We'll be singing "Baraye"

--- src/Client.ts
+++ src/Client.ts
@@ -6,9 +6,10 @@ import { Listener } from "./client/Listener";
 import { ProtocolValidator } from "./vendor/ProtocolValidator";
 import { PacketSorter } from "./vendor/PacketSorter";
 import { PacketEncryptor } from "./vendor/PacketEncryptor";
-import { DataPacket, RequestNetworkSettingsPacket, TextPacket, TextPacketType, Vector3f } from "@serenityjs/protocol";
+import { DataPacket, InputDataFlags, InputMode, InteractionMode, PlayerAuthInputData, PlayerAuthInputPacket, PlayMode, RequestNetworkSettingsPacket, TextPacket, TextPacketType, Vector2f, Vector3f } from "@serenityjs/protocol";
 import { Priority } from "@serenityjs/raknet";
 import { PluginLoader } from "./vendor/PluginLoader";
+import { Inventory } from "./client/inventory/Inventory";
 
 class Client extends Listener {
     public readonly raknet: RakNetClient;
@@ -21,11 +22,19 @@ class Client extends Listener {
     public runtimeEntityId!: bigint;
     public username!: string;
     public position!: Vector3f;
+    public tick: number = 0;
 
     public playStatus!: number;
     public _encryption: boolean = false;
     public _encryptor!: PacketEncryptor;
 
+    private headYaw: number = 0;
+    private pitch: number = 0;
+    private yaw: number = 0;
+    private velocity: Vector3f = new Vector3f(0, 0, 0);
+
+    public inventory: Inventory;
+    
     constructor(options: Partial<ClientOptions> = {}) {
         super();
         this.options = { ...defaultOptions, ...options };
@@ -35,20 +44,26 @@ class Client extends Listener {
         this.packetSorter = new PacketSorter(this);
         this.pluginLoader = new PluginLoader(this);
         this.prepare();
+        this.inventory = new Inventory(this);
+
+        setInterval(() => { this.emit("tick", this.tick++) }, 50)
+        this.on("spawn", this.handleAuthInput.bind(this))
     }
 
     public async connect(): Promise<void> {
         const protocolValidator = new ProtocolValidator(this);
-        //console.time('Protocol Validation');
-        await protocolValidator.validateAndInstall();
-        //console.timeEnd('Protocol Validation');
-        await this.pluginLoader.init();
+        if(this.options.validateProtocol) {
+            await protocolValidator.validateAndCheck();
+        }
+        if(this.options.loadPlugins) {
+            await this.pluginLoader.init();
+        }
         this.initializeSession();
     }
 
     private initializeSession(): void {
-        this.options.offline ? createOfflineSession(this) : authenticate(this);
         this.on("session", this.handleSessionStart.bind(this));
+        this.options.offline ? createOfflineSession(this) : authenticate(this);
     }
 
     private handleSessionStart(): void {
@@ -81,7 +96,42 @@ class Client extends Listener {
         }
     }
 
-    sendMessage(text: string): void {
+    private handleAuthInput(): void {
+        setInterval(() => {
+            const inputData = new PlayerAuthInputData();
+            inputData.setFlag(InputDataFlags.BlockBreakingDelayEnabled, true);
+
+            let packet = new PlayerAuthInputPacket();
+            packet.analogueMoveVector = new Vector2f(this.velocity.x, this.velocity.z);
+            packet.blockActions = [];
+            packet.gazeDirection = undefined;
+            packet.headYaw = this.headYaw;
+            packet.inputData = inputData;
+            packet.inputMode = InputMode.Mouse;
+            packet.itemStackRequest = undefined;
+            packet.motion = new Vector2f(this.velocity.x, this.velocity.z);
+            packet.pitch = this.pitch;
+            packet.playMode = PlayMode.Screen;
+            packet.interactionMode = InteractionMode.Touch;;
+            packet.position = this.position;
+            packet.positionDelta = new Vector3f( 
+                this.velocity.x,
+                this.velocity.y,
+                this.velocity.z
+            );
+            packet.tick = BigInt(this.tick);
+            packet.transaction = undefined;
+            packet.yaw = this.yaw;
+
+            let cancel = false;
+            this.emit("PrePlayerAuthInputPacket", packet, cancel);
+            if(!cancel) {
+                this.sendPacket(packet);
+            }
+        }, 100)
+    }
+
+    public sendMessage(text: string): void {
         const textPacket = new TextPacket();
         textPacket.filtered = '';
         textPacket.message = text.replace(/^\s+/, '');
@@ -93,6 +143,25 @@ class Client extends Listener {
         textPacket.xuid = "";
         this.sendPacket(textPacket, Priority.Normal);
     }
+
+    /**
+     * This ownt look at the block but a direction atleast (something i needed for now)
+     */
+    public lookAt(x: number, y: number, z: number): void { 
+        const deltaX = x - this.position.x;
+        const deltaY = y - this.position.y;
+        const deltaZ = z - this.position.z;
+
+        const groundDistance = Math.sqrt(deltaX * deltaX + deltaZ * deltaZ);
+        const pitch = Math.atan2(deltaY, groundDistance) * (180 / Math.PI);
+        const yaw = Math.atan2(-deltaX, -deltaZ) * (180 / Math.PI);
+        const headYaw = yaw;
+
+        this.pitch = pitch;
+        this.yaw = yaw;
+        this.headYaw = headYaw;
+    }
+
 }
 
 export { Client };
--- src/tools/test.ts
+++ src/tools/test.ts
@@ -1,37 +1,68 @@
-import { PlayerAuthInputData, PlayerAuthInputPacket, SetTimePacket, TextPacket } from "@serenityjs/protocol";
+import { RespawnPacket, RespawnState, TextPacket, Vector3f } from "@serenityjs/protocol";
 import { Client } from "../Client";
 import { Logger } from "../vendor/Logger";
+import { Priority } from "@serenityjs/raknet";
 
 const client = new Client({
     host: "127.0.0.1",
     offline: false,
-    username: "SanctumTerra", // Use Your own Username
-    version: "1.21.20"
-})   
-client.connect();
-console.time("SpawnTime");
+    username: "SanctumTerra",
+    version: "1.21.20",
+    port: 19133,
+});
+
+try {
+    client.connect();
+    console.time("SpawnTime");
+} catch (error) {
+    console.error(error);
+}
 
 client.on("spawn", () => {
     console.timeEnd("SpawnTime");
+   
+    setInterval(() => {
+        const [x, y, z] = Math.random() < 0.5 ? [258, 74, 257] : [251, 72, 257];
+        client.lookAt(x, y, z);
+    }, 1000);
+
     setInterval(async () => {
-        Logger.info(`§l§a`, client.position.x, client.position.y, client.position.z)
-    }, 2500)
+        const item = client.inventory.items.find(item => item.network > 0);
+        if (item) {
+            client.inventory.dropItem(client.inventory.items.indexOf(item), 1);
+            await new Promise(resolve => setTimeout(resolve, 250));
+        }
+    }, 250);
 });
 
 client.on("SetTimePacket", () => {
-    client.sendMessage("hey")
+    const { x, y, z } = client.position;
+    client.sendMessage(`Hello! My position is ${Math.floor(x)} ${Math.floor(y - 1.62)} ${Math.floor(z)}`);
 });
 
 client.on("TextPacket", (packet: TextPacket): void => {
-    if(packet.parameters){
-        if(packet.message.includes("chat.type.text")) return Logger.chat(`§f<${packet.parameters[0]}> ${packet.parameters[1]}`)
-        if(packet.message.includes("multiplayer.player.joined")) return Logger.chat(`§e${packet.parameters[0]} §ejoined the game`)
-        if(packet.message.includes("multiplayer.player.left")) return Logger.chat(`§f${packet.parameters[0]} §7left the game`)
-        console.log(packet.message);
+    if (!packet.parameters) return Logger.chat(packet.message);
+
+    const [param1, param2] = packet.parameters;
+    if (packet.message.includes("chat.type.text")) Logger.chat(`§f<${param1}> ${param2}`);
+    else if (packet.message.includes("multiplayer.player.joined")) Logger.chat(`§e${param1} §ejoined the game`);
+    else if (packet.message.includes("multiplayer.player.left")) Logger.chat(`§f${param1} §7left the game`);
+    else console.log(packet.message);
+});
+
+client.on("RespawnPacket", (packet: RespawnPacket) => {
+    const response = new RespawnPacket();
+    response.runtimeEntityId = client.runtimeEntityId;
+
+    if (packet.state === RespawnState.ServerSearchingForSpawn) {
+        response.position = packet.position;
+        response.state = RespawnState.ClientReadyToSpawn;
+    } else if (packet.state === RespawnState.ServerReadyToSpawn) {
+        response.position = new Vector3f(0, 0, 0);
+        // @ts-expect-error So far this does not work NOTE!: prob missing a state?
+        response.state = 3;
     }
-    Logger.chat(packet.message);
-})
 
-Logger.info("§l§bINFO?§6!");
-Logger.error("§l§cERROR§6!")
-
+    client.position = packet.position;
+    client.sendPacket(response, Priority.Immediate);
+});
