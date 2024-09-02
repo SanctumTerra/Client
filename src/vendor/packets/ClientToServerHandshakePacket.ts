42cc12341f9957eca27da80cc2431c0f3a3c99de
AnyBananaGAME <ainaraspirmas@gmail.com>
2024-09-02 21:10:33 +0300

    Now in Serenity itself.

--- src/vendor/packets/ClientToServerHandshakePacket.ts
+++
@@ -1,7 +0,0 @@
-import { DataPacket } from "@serenityjs/protocol"
-import { Proto } from "@serenityjs/raknet";
-
-
-
-@Proto(Number(0x04))
-export class ClientToServerHandshakePacket extends DataPacket {}
